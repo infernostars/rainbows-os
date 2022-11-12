a465e86809cd7830b73b52b0139d0e01225ec4f2
infernostars <blazem7708.2@gmail.com>
2022-11-12 13:31:04 -0700

    update to add more utils

--- kernel/pic.c
+++ kernel/pic.c
@@ -110,22 +110,21 @@ void IRQ_clear_mask(unsigned char IRQline) {
     }
     value = inb(port) & ~(1 << IRQline);
     outb(port, value);    
+}
 
-#define PIC1_CMD                    0x20
-#define PIC1_DATA                   0x21
-#define PIC2_CMD                    0xA0
-#define PIC2_DATA                   0xA1
+
 #define PIC_READ_IRR                0x0a    /* OCW3 irq ready next CMD read */
 #define PIC_READ_ISR                0x0b    /* OCW3 irq service next CMD read */
  
+
 /* Helper func */
 static uint16_t __pic_get_irq_reg(int ocw3)
 {
     /* OCW3 to PIC CMD to get the register values.  PIC2 is chained, and
      * represents IRQs 8-15.  PIC1 is IRQs 0-7, with 2 being the chain */
-    outb(PIC1_CMD, ocw3);
-    outb(PIC2_CMD, ocw3);
-    return (inb(PIC2_CMD) << 8) | inb(PIC1_CMD);
+    outb(PIC1_COMMAND, ocw3);
+    outb(PIC2_COMMAND, ocw3);
+    return (inb(PIC2_COMMAND) << 8) | inb(PIC1_COMMAND);
 }
  
 /* Returns the combined value of the cascaded PICs irq request register */
